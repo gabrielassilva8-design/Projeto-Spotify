66aa2cfc485f82a980b8b63ecac60c62e06f922f
GabsAsecret07 - Design <gabriela.s.silva8@ba.estudante.senai.br>
2026-03-11 20:11:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const albumUm = document.querySelector('.album-um');
+    const album1 = document.createElement('img');
+    album1.classList.add('album-card1');
+    album1.src = 'album-chris grey.jpg';
+    albumUm.appendChild(album1);
+
+    const albumDois = document.querySelector('.album-dois');
+    const album2 = document.createElement('img');
+    album2.classList.add('album-card2');
+    album2.src = 'album-ariana grande.jpg';
+    albumDois.appendChild(album2);
+
+    const albumTres = document.querySelector('.album-tres');
+    const album3 = document.createElement('img');
+    album3.classList.add('album-card3');
+    album3.src = 'album-alok.avif';
+    albumTres.appendChild(album3);
+
+    const albumQuatro = document.querySelector('.album-quatro');
+    const album4 = document.createElement('img');
+    album4.classList.add('album-card4');
+    album4.src = 'album-rihanna.jpg';
+    albumQuatro.appendChild(album4);
+
+     const albumCinco = document.querySelector('.album-cinco');
+    const album5 = document.createElement('img');
+    album5.classList.add('album-card5');
+    album5.src = 'album-duquesa.jpg';
+    albumCinco.appendChild(album5);
+
+    const artistaUm = document.querySelector('.artista-um');
+    const artista1 = document.createElement('img');
+    artista1.classList.add('artist-card1');
+    artista1.src = 'chris grey.avif';
+    artistaUm.appendChild(artista1);
+
+    const artistaDois = document.querySelector('.artista-dois');
+    const artista2 = document.createElement('img');
+    artista2.classList.add('artist-card2');
+    artista2.src = 'ariana grande.jpg';
+    artistaDois.appendChild(artista2);
+
+    const artistaTres = document.querySelector('.artista-tres');
+    const artista3 = document.createElement('img');
+    artista3.classList.add('artist-card3');
+    artista3.src = 'alok.jpg';
+    artistaTres.appendChild(artista3);
+
+    const artistaQuatro = document.querySelector('.artista-quatro');
+    const artista4 = document.createElement('img');
+    artista4.classList.add('artist-card4');
+    artista4.src = 'duquesa.jpg';
+    artistaQuatro.appendChild(artista4);
+
+    const artistaCinco = document.querySelector('.artista-cinco');
+    const artista5 = document.createElement('img');
+    artista5.classList.add('artist-card5');
+    artista5.src = 'rihanna.jpg';
+    artistaCinco.appendChild(artista5);
+
+});
+        
+        
+    
+
+
+
+
+
